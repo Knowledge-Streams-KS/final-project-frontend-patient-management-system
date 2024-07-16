26743bdd75c59a186e5e363832997b9c8054d0da
Maryam Saba <maryams91101@gmail.com>
2024-07-17 00:52:00 +0500

    Handling Dashboards

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import DefaultLayout from './Layouts/DefaultLayout'
 import Appointment from './Pages/Appointment'
-import Doctors from './Pages/Doctors'
+import Doctors from './Pages/DoctorDashBoard'
 import FAQ from './Pages/FAQ'
 import Departments from './Pages/Departments'
 //toastify -> displaying alerts 
@@ -14,6 +14,7 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 import SignUp from './Pages/SignUp'
 import PatientDashBoard from './Pages/PatientDashBoard'
+import PatientLayout from './Layouts/PatientLayout'
 
 function App() {
 
@@ -24,12 +25,12 @@ function App() {
   
       <Routes>
         <Route path='/' element = {<DefaultLayout><Home/></DefaultLayout>}/>
-        <Route path='/Appointment' element = {<DefaultLayout><Appointment/></DefaultLayout>}/>
+        <Route path='/Appointment' element = {<PatientLayout><Appointment/></PatientLayout>}/>
         <Route path='/Doctors' element = {<DefaultLayout><Doctors/></DefaultLayout>}/>
-        <Route path='/FAQ' element = {<DefaultLayout><FAQ/></DefaultLayout>}/>
+        <Route path='/FAQ' element = {<PatientLayout><FAQ/></PatientLayout>}/>
         <Route path='/Departments' element = {<DefaultLayout><Departments/></DefaultLayout>}/>
         <Route path='/SignUp' element = {<SignUp/>} />
-        <Route path='/PatientDashBoard' element = {<PatientDashBoard/>}/>
+        <Route path='/PatientDashBoard' element = {<PatientLayout><PatientDashBoard/></PatientLayout>}/>
       </Routes> 
       <ToastContainer position='top-center'/>
   
---
+++ src/Components/PatientHeader.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+const PatientHeader = () => {
+  return (
+ 
+       <header className='flex justify-between bg-black text-white top-0 w-full left-0 fixed  z-50 p-4'>
+        <a href="">Hi</a>
+        <nav className='flex justify-end'>
+            <ul className='flex space-x-7 p-3  '>
+                {/* <li><NavLink to= "/">Home</NavLink></li> */}
+                <li><NavLink to="/Appointment">Appointment</NavLink></li>
+{/*               
+             
+                <li><NavLink to="/FAQ">FAQ</NavLink></li> */}
+                <li><NavLink to="/SignOutPatient">SignOut As Patient</NavLink></li>
+            </ul>
+        </nav>
+       </header>
+      )
+    }
+
+
+
+export default PatientHeader
